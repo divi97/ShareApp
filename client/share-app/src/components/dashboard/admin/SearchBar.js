f2ac2e317f53a6e1ec293631a8e1dc2bb3c952a0
divyansh <37957706+divi97@users.noreply.github.com>
2020-08-20 02:28:01 +0530

    Search Box

--- client/share-app/src/components/dashboard/admin/SearchBar.js
+++ client/share-app/src/components/dashboard/admin/SearchBar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Container from '@material-ui/core/Container'
+import './Search.css';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function SearchBar () {
     return (
         <>
-        <div>Search BAR</div>
+        <Container>
+        <nav className='header'>
+        <div className='header_search'>
+            <input type='text' className='header_searchInput' />
+            <SearchIcon className='header_searchIcon'/>
+        </div>
+        </nav>
+        </Container>
+        {/* <div>Search BAR</div> */}
         </>
     )    
 }
